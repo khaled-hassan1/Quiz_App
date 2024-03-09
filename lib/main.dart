a2577372d8b9476720403d9ffde3f45f75259a5c
Khaled Ghaly <112872753+khaled-hassan1@users.noreply.github.com>
2024-03-09 18:51:35 +0200

    modify Comment

--- lib/main.dart
+++ lib/main.dart
@@ -197,7 +197,7 @@ class MyApp extends StatelessWidget {
                 fontSize: 18,
                 fontFamily: 'Marhey',
                 color: AppSettings.white,
-                fontWeight: AppSettings.bold, //new comment
+                fontWeight: AppSettings.bold, 
               )),
         ),
       ),
